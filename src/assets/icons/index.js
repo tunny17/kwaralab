4583216e69ba70835d8fbe346b9bd0b9895a96bd
tunny17 <tvnji17@gmail.com>
2023-01-27 19:36:07 +0100

    completion of the hero component

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,1 +1,3 @@
-export { default as Logo } from './Logo.svg'
+export { default as Logo } from './Logo.svg';
+export { default as Dashboard } from './Dashboard.jpg';
+export { default as Chats } from './Chats.jpg';
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Dashboard, Chats } from '../assets/icons'
 
 const Hero = () => {
   return (
     <div    className='hero-container'>
-        <div className='hero-text   --display-flex'>
-            <h1>Living a Healthy and Longer Life</h1>
-            <p>Receive exceptional healthcare services such as urgent care, therapy & integrative medical care</p>
-            <button type='button'>Book Now</button>
-        </div>
+      <div className='hero-text   --display-flex'>
+        <h1 className='thicccboi-medium'>Living a Healthy and Longer Life</h1>
+        <p  className='thicccboi-light'>Receive exceptional healthcare services such as urgent care, therapy & integrative medical care</p>
+        <button type='button' className='thicccboi-medium'>Book Now</button>
+      </div>
+      <div  className='image-container'>
+        <img src={Dashboard} alt="A dashboard"  className='dashboard' />
+        <img src={Chats} alt="A chat log" className='chat-log' />
+      </div>
     </div>
   )
 }
